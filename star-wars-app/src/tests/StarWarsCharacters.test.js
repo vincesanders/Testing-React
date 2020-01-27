18846f5b64ebbea6b2d1917b4b8144bbf746b50d
Vincent Sanders <vincesanders@hotmail.com>
2020-01-27 17:00:13 -0600

    Added test to check for character displaying after an axios call.

--- star-wars-app/src/tests/StarWarsCharacters.test.js
+++ star-wars-app/src/tests/StarWarsCharacters.test.js
@@ -4,16 +4,35 @@ import { render, wait, fireEvent } from '@testing-library/react';
 import { getData as mockGetNewData } from '../api';
 
 //creates a mock function for everything exported from ../api
-// jest.mock('../api');
+jest.mock('../api');
 
 test('Renders the StarWarsCharacters component with character list and functioning next and previous buttons', async () => {
+
+    const response = {
+        next: 'nextURL',
+        previous: 'previousURL',
+        results: [
+            {
+                url: 'characterURL',
+                name: 'characterName'
+            }
+        ]
+    }
+
+    mockGetNewData.mockResolvedValueOnce(response);
+
     const wrapper = render(<StarWarsCharacters />);
 
     const nextButton = wrapper.getByText(/next/i);
     const previousButton = wrapper.getByText(/previous/i);
 
+
+
     fireEvent.click(nextButton);
     fireEvent.click(previousButton);
 
-    
+    await wait(() => {
+        //make sure after the api call is made that a character with characterName appears on the page.
+        expect(wrapper.getByText(/charactername/i)).toBeTruthy();
+    });
 });
